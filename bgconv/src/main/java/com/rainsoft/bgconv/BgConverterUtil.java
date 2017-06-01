f7e3c55f419596f0a33aad7ac8d104a2a1da9982
smoker21 <chen.lance@gmail.com>
2017-06-01 23:55:34 +0800

    1.Modify bugs. Silly typo.

--- bgconv/src/main/java/com/rainsoft/bgconv/BgConverterUtil.java
+++ bgconv/src/main/java/com/rainsoft/bgconv/BgConverterUtil.java
@@ -10,25 +10,30 @@ import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Lance
  *
  */
 @SuppressWarnings("resource")
 public class BgConverterUtil {
+	private final static Logger logger = LoggerFactory.getLogger(BgConverterUtil.class);
+	
 	private final static HashMap<ByteBuffer, byte[]> bgMap = new HashMap<>();
 	private final static HashMap<ByteBuffer, byte[]> gbMap = new HashMap<>();
-
+	
 	static {
 		InputStream inbg = ClassLoader.getSystemResourceAsStream("bg2gb.map");
 		Scanner scannerbg = new Scanner(new InputStreamReader(inbg));
+		logger.trace("Reader mapping table");
 		while (scannerbg.hasNext()) {
 			String s = scannerbg.nextLine();
 			String[] innerCodes = s.split(" ");
-			byte[] bg5key = getBytesFromString(innerCodes[0]);
-//			System.out.println("bg5:" + (int)bg5key[0] + ":" + (int)bg5key[1]) ;
-			byte[] gbkey = getBytesFromString(innerCodes[1]);
-//			System.out.println("gbk:" + (int)gbkey[0] + ":" + (int)gbkey[1]);
+			byte[] bg5key = getByteFromInnercode(innerCodes[0]);
+			byte[] gbkey = getByteFromInnercode(innerCodes[1]);
+			logger.trace("big5:" + (int)bg5key[0] + ":" + (int)bg5key[1] + " -- > " + (int)gbkey[0] + ":" + (int)gbkey[1]);
 			bgMap.put(ByteBuffer.wrap(bg5key), gbkey);			
 		}
 		scannerbg.close();		
@@ -37,17 +42,15 @@ public class BgConverterUtil {
 		while (scannergb.hasNext()) {
 			String s = scannergb.nextLine();
 			String[] innerCodes = s.split(" ");
-			byte[] gbkey = getBytesFromString(innerCodes[0]);
-//			System.out.println("bg5:" + (int)bg5key[0] + ":" + (int)bg5key[1]) ;
-			byte[] big5key = getBytesFromString(innerCodes[1]);
-//			System.out.println("gbk:" + (int)gbkey[0] + ":" + (int)gbkey[1]);
-			bgMap.put(ByteBuffer.wrap(gbkey), big5key);			
+			byte[] gbkey = getByteFromInnercode(innerCodes[0]);
+			byte[] big5key = getByteFromInnercode(innerCodes[1]);
+			logger.trace("gbk:" + + (int)gbkey[0] + ":" + (int)gbkey[1] + " -- > " + (int)big5key[0] + ":" + (int)big5key[1]);
+			gbMap.put(ByteBuffer.wrap(gbkey), big5key);			
 		}
 		scannerbg.close();		
 	}
 
 	public static String b2gRawString(String source) throws UnsupportedEncodingException {
-		System.out.println(bgMap.size());
 		StringBuilder sb = new StringBuilder(); 
 		byte[] src = source.getBytes("MS950");
 		for (int start=0; start <src.length; ) {
@@ -60,13 +63,13 @@ public class BgConverterUtil {
 			Byte c1 = src[start];
 			Byte c2 = src[start+1];
 			
-			System.out.println("src byte:" + c1.intValue() + ":" + c2.intValue());
+			logger.trace("src byte:" + c1.intValue() + ":" + c2.intValue());
 			
 			byte[] dest = bgMap.get(ByteBuffer.wrap(new byte[] {src[start],src[start+1]}));
 			if (dest !=null) {
 				Byte d1 = dest[0];
 				Byte d2 = dest[1];
-				System.out.println("dest byte:" + d1.intValue() + ":" + d2.intValue());
+				logger.trace("dest byte:" + d1.intValue() + ":" + d2.intValue());
 				sb.append(new String(dest,"GBK"));
 			} 
 			start++; 
@@ -76,9 +79,8 @@ public class BgConverterUtil {
 	}
 	
 	public static String g2bRawString(String source) throws UnsupportedEncodingException {
-		System.out.println(gbMap.size());
 		StringBuilder sb = new StringBuilder(); 
-		byte[] src = source.getBytes("MS950");
+		byte[] src = source.getBytes("GBK");
 		for (int start=0; start <src.length; ) {
 			if (src[start] >0 ) {				
 				char c = (char) src[start];
@@ -89,13 +91,13 @@ public class BgConverterUtil {
 			Byte c1 = src[start];
 			Byte c2 = src[start+1];
 			
-			System.out.println("src byte:" + c1.intValue() + ":" + c2.intValue());
+			logger.trace("src byte:" + c1.intValue() + ":" + c2.intValue());
 			
 			byte[] dest = gbMap.get(ByteBuffer.wrap(new byte[] {src[start],src[start+1]}));
 			if (dest !=null) {
 				Byte d1 = dest[0];
 				Byte d2 = dest[1];
-				System.out.println("dest byte:" + d1.intValue() + ":" + d2.intValue());
+				logger.trace("dest byte:" + d1.intValue() + ":" + d2.intValue());
 				sb.append(new String(dest,"MS950"));
 			} 
 			start++;start++; 
@@ -110,16 +112,16 @@ public class BgConverterUtil {
 		
 		while (scanner.hasNext()) {
 			String s = scanner.nextLine();
-			if (getMappingFromString(s) == "") {
+			if (getMapCharacterFromInnerCode(s) == "") {
 				continue;
 			}
-			sb.append(getMappingFromString(s) + System.lineSeparator());
+			sb.append(getMapCharacterFromInnerCode(s) + System.lineSeparator());
 		}
 		scanner.close();
 		return sb.toString();
 	}
 
-	public static String getMapping4Big5(byte bytes[]) {
+	public static String getCharacter4Big5(byte bytes[]) {
 		String s = "";
 		try {
 			s = new String(bytes, "MS950");
@@ -128,8 +130,13 @@ public class BgConverterUtil {
 		}
 		return s;
 	}
-
-	public static String getMapping4Gbk(byte bytes[]) {
+	
+	/**
+	 * give
+	 * @param bytes
+	 * @return
+	 */
+	public static String getCharacter4Gbk(byte bytes[]) {
 		String s = "";
 		try {
 			s = new String(bytes, "GBK");
@@ -138,8 +145,13 @@ public class BgConverterUtil {
 		}
 		return s;
 	}
-
-	public static byte[] getBytesFromString(String str) {
+	
+	/**
+	 * Get byte array from inner code definition
+	 * @param str - GBK "C3B4", Big5 "A45C"
+	 * @return
+	 */
+	public static byte[] getByteFromInnercode(String str) {
 		if (str.length() != 4)
 			throw new RuntimeException("String format error [" + str + "] Length should be 4.");
 		byte bytes[] = new byte[2];
@@ -150,26 +162,27 @@ public class BgConverterUtil {
 		bytes[1] = (byte) v2;
 		return bytes;
 	}
-
-	public String getMappingFromString(String str) {
+	
+	
+	/**
+	 * Read from definition innercode and translate to chinese character
+	 * innercode finding : http://bianma.supfree.net/chaye.asp?id=4E48
+	 * @param str - "MS950 GBK" for ex: BBD5 B8F3
+	 * @return
+	 */
+	public static String getMapCharacterFromInnerCode(String str) {
 		String[] innerCodes = str.split(" ");
 		if (innerCodes[1].equalsIgnoreCase("2020")) {
 			return "";
 		}
 		StringBuilder sb = new StringBuilder();
 		sb.append(innerCodes[0] + ":");
-		sb.append(getMapping4Big5(getBytesFromString(innerCodes[0])));
+		sb.append(getCharacter4Big5(getByteFromInnercode(innerCodes[0])));
 		sb.append(":");
 		sb.append(innerCodes[1] + ":");
-		sb.append(getMapping4Gbk(getBytesFromString(innerCodes[1])));
+		sb.append(getCharacter4Gbk(getByteFromInnercode(innerCodes[1])));
 		return sb.toString();
 	}
 
-	private void createByteMapping(String str) {
-		String[] innerCodes = str.split(" ");
-		byte[] bg5key = getBytesFromString(innerCodes[0]);
-		byte[] gbkey = getBytesFromString(innerCodes[1]);
-		bgMap.put(ByteBuffer.wrap(bg5key), gbkey);
-	}
 
 }
--- bgconv/src/test/java/com/rainsoft/bgconv/BgConverterUtilTest.java
+++ bgconv/src/test/java/com/rainsoft/bgconv/BgConverterUtilTest.java
@@ -7,92 +7,135 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Lance
  *
  */
 public class BgConverterUtilTest {
-
+	private final static org.slf4j.Logger logger = LoggerFactory.getLogger("BgConverterUtilTest");
+	static {
+		Logger log = LogManager.getLogManager().getLogger("");
+		for (Handler h : log.getHandlers()) {
+		    h.setLevel(Level.FINEST);
+		}			
+	}
 	/**
-	 * Test method for {@link com.rainsoft.bgconv.BgConverterUtil#getMapping4Big5(byte[])}.
-	 * F9F6 A86C
+	 * Test method for
+	 * {@link com.rainsoft.bgconv.BgConverterUtil#getCharacter4Big5(byte[])}. F9F6
+	 * A86C
 	 **/
 	@Test
 	public void testGetMapping() {
-		byte bytes[]= new byte[2]; 
+		byte bytes[] = new byte[2];
 		int val1 = 0xF9;
-		int val2 = 0xF6; 	
-		byte a = (byte)val1;
-		byte b = (byte)val2;		
+		int val2 = 0xF6;
+		byte a = (byte) val1;
+		byte b = (byte) val2;
 		System.out.println("xxx" + (int) a + ":" + (int) b);
-		bytes[0] = a;  
-		bytes[1] = b;	
-		String s =BgConverterUtil.getMapping4Big5(bytes);
-		System.out.println(s);
+		bytes[0] = a;
+		bytes[1] = b;
+		String s = BgConverterUtil.getCharacter4Big5(bytes);
+		logger.info(s);
 		assertNotNull(s);
-		int v1= 0xA8; 
-		int v2= 0x6C; 
-		byte c = (byte)v1; 
-		byte d = (byte)v2;
-		byte bytes2[]= new byte[2];
-		bytes2[0] = c; 
-		bytes2[1] = d; 
-		HashMap<ByteBuffer,byte[]> map = new HashMap<>();
+		assertEquals("╨",s);
+		int v1 = 0xA8;
+		int v2 = 0x6C;
+		byte c = (byte) v1;
+		byte d = (byte) v2;
+		byte bytes2[] = new byte[2];
+		bytes2[0] = c;
+		bytes2[1] = d;
+		HashMap<ByteBuffer, byte[]> map = new HashMap<>();
 		map.put(ByteBuffer.wrap(bytes), bytes2);
-		
-		String s2 = BgConverterUtil.getMapping4Gbk(bytes2);
-		System.out.println(s2);
-		byte bytes3[]= new byte[2]; 		
+
+		String s2 = BgConverterUtil.getCharacter4Gbk(bytes2);
+		assertEquals("╨",s2);
+		byte bytes3[] = new byte[2];
 		int va1 = 0xF9;
-		int va2 = 0xF6; 		
-		byte va = (byte)val1;
-		byte vb = (byte)val2;
-		bytes3[0] = va;  
+		int va2 = 0xF6;
+		byte va = (byte) val1;
+		byte vb = (byte) val2;
+		bytes3[0] = va;
 		bytes3[1] = vb;
 		byte[] o = map.get(ByteBuffer.wrap(bytes3));
 		assertEquals(o, bytes2);
 	}
 
-//	@Test
+	@Test
 	public void testGetBytesFromString() {
-//		String s = "A3BF A841"; 
 		String s = "F07C F1F9";
-		BgConverterUtil util = new BgConverterUtil();
-		System.out.println(util.getMappingFromString(s));
+		logger.info(BgConverterUtil.getMapCharacterFromInnerCode(s));
 	}
-	
-//	@Test
+
+	// @Test
 	public void testReadDefinitionFile() {
 		BgConverterUtil util = new BgConverterUtil();
 		String s = util.readDefinitionFile();
 		System.out.println(s);
 	}
-//		
+
 	@Test
 	public void testDirectChange() throws UnsupportedEncodingException {
+	
 		InputStream in = ClassLoader.getSystemResourceAsStream("big5test");
-		Scanner scanner = new Scanner(new InputStreamReader(in,"MS950"));		
+		Scanner scanner = new Scanner(new InputStreamReader(in, "MS950"));
 		while (scanner.hasNext()) {
 			try {
 				String s = scanner.nextLine();
-				System.out.println(s);
 				String source = new String(s.getBytes());
-				System.out.println("source:" + source);
+				logger.info("source:" + source);
 				String result = BgConverterUtil.b2gRawString(source);
-				System.out.println("result:" + result);
+				logger.info("result:" + result);
+				String result2 = BgConverterUtil.g2bRawString(source);
+				logger.info("result2:" + result2);
 			} catch (UnsupportedEncodingException e) {
-				fail(e.getMessage()); 
+				fail(e.getMessage());
 			}
 		}
 		scanner.close();
+	}
+
+	@Test
+	public void testDirectChangeG2B() throws UnsupportedEncodingException {
+		Logger log = LogManager.getLogManager().getLogger("");
+		for (Handler h : log.getHandlers()) {
+		    h.setLevel(Level.FINEST);
+		}
+		InputStream in = ClassLoader.getSystemResourceAsStream("gbktest.txt");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in, "GBK"));
+		try {
+			while (reader.ready()) {
+				String source = reader.readLine();
+				logger.info("source:" + source);
+				String result = BgConverterUtil.g2bRawString(source);
+				logger.info("result:" + result);
+				String result2 = BgConverterUtil.b2gRawString(result);
+				logger.info("result2:" + result2);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+			}
+		}
+		
 	}
 }
